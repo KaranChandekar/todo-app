9cf4f865354e60f262ddcd83c14bff94ad5eb6ab
Karan Chandekar <karanchandekar431@gmail.com>
2023-07-05 14:24:06 +0530

    feat: create api using json server and store the data

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,18 +1,26 @@
-const TodoList = () => {
+import { ITask } from "@/types/tasks";
+
+interface TodoListProps {
+  tasks: ITask[];
+}
+
+const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
   return (
     <div>
       <table className="w-full text-left border-collapse">
         <thead className="bg-gray-200">
           <tr>
-            <th className="p-4">Name</th>
-            <th className="p-4">Desc</th>
+            <th className="p-4">Tasks</th>
+            <th className="p-4">Actions</th>
           </tr>
         </thead>
         <tbody className="text-gray-700">
-          <tr>
-            <td className="p-4">Play</td>
-            <td className="p-4">Football</td>
-          </tr>
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td className="p-4">{task.text}</td>
+              <td className="p-4">Football</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
+import { getAllTodos } from "@/api";
 import AddTask from "./components/AddTask";
 import TodoList from "./components/TodoList";
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await getAllTodos();
+  console.log(tasks);
+
   return (
     <main className="max-w-4xl mx-auto mt-6">
       <div className="flex flex-col gap-4 my-5 text-center">
         <h1 className="text-2xl font-bold">Task Management App</h1>
         <AddTask />
-        <TodoList />
+        <TodoList tasks={tasks} />
       </div>
     </main>
   );
