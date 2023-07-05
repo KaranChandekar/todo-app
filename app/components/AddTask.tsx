045a4efab3b58835ac2ace0f8674f937088914e9
Karan Chandekar <karanchandekar431@gmail.com>
2023-07-05 15:58:55 +0530

    install: uuid for unique id

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -4,6 +4,7 @@ import Modal from "./Modal";
 import { FormEventHandler, useState } from "react";
 import { addTodo } from "@/api";
 import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
 
 const AddTask = () => {
   const router = useRouter();
@@ -13,7 +14,7 @@ const AddTask = () => {
   const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
     await addTodo({
-      id: "5",
+      id: uuidv4(),
       text: newTaskValue,
     });
     setNewTaskValue("");
