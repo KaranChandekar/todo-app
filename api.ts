f0d363f9d17fe8cfebc4bdc6d17264908fff8376
Karan Chandekar <karanchandekar431@gmail.com>
2023-07-06 13:14:58 +0530

    feat: delete task with deleteTodo api

--- api.ts
+++ api.ts
@@ -31,3 +31,9 @@ export const editTodo = async (todo: ITask): Promise<ITask> => {
   const updatedTodo = await res.json();
   return updatedTodo;
 };
+
+export const deleteTodo = async (id: string): Promise<void> => {
+  await fetch(`${baseUrl}/tasks/${id}`, {
+    method: "DELETE",
+  });
+};
--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -5,7 +5,7 @@ import { FormEventHandler, useState } from "react";
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 import Modal from "./Modal";
 import { useRouter } from "next/navigation";
-import { editTodo } from "@/api";
+import { deleteTodo, editTodo } from "@/api";
 
 interface TaskProps {
   task: ITask;
@@ -28,6 +28,12 @@ const Task: React.FC<TaskProps> = ({ task }) => {
     router.refresh();
   };
 
+  const handleDeleteTask = async (id: string) => {
+    await deleteTodo(id);
+    setOpenModalDeleted(false);
+    router.refresh();
+  };
+
   return (
     <tr key={task.id} className="border-b border-solid">
       <td className="w-full p-4">{task.text}</td>
@@ -38,7 +44,6 @@ const Task: React.FC<TaskProps> = ({ task }) => {
           size={20}
           className="text-blue-500"
         />
-
         <Modal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
           <form onSubmit={handleSubmitEditTodo}>
             <h3 className="text-lg font-bold text-gray-700">Edit Task</h3>
@@ -60,7 +65,47 @@ const Task: React.FC<TaskProps> = ({ task }) => {
           </form>
         </Modal>
 
-        <FiTrash2 cursor="pointer" size={20} className="text-red-500" />
+        <FiTrash2
+          onClick={() => setOpenModalDeleted(true)}
+          cursor="pointer"
+          size={20}
+          className="text-red-500"
+        />
+        <Modal modalOpen={openModalDeleted} setModalOpen={setOpenModalDeleted}>
+          <svg
+            className="w-8 h-8 mx-auto mb-4 text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+          <h3 className="mb-5 text-lg font-normal text-center text-gray-7000">
+            Are you sure you want to delete this task?
+          </h3>
+          <div className="flex items-center justify-center gap-2">
+            <button
+              onClick={() => handleDeleteTask(task.id)}
+              type="submit"
+              className="text-white bg-red-600 hover:bg-red-700 focus:outline-none font-semibold rounded-md px-4 py-2.5"
+            >
+              Yes, I'm sure
+            </button>
+            <button
+              className="px-4 py-2.5 font-semibold text-white capitalize ease-in-out focus:outline-none bg-purple-600 rounded-md hover:bg-purple-700"
+              type="submit"
+            >
+              No, cancel
+            </button>
+          </div>
+        </Modal>
       </td>
     </tr>
   );
